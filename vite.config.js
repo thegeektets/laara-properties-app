52ce2d352d1930d3fe0c2b5b562c093f1d114d3f
Griffin Muteti <griffinmuteti31@gmail.com>
2024-11-17 16:41:55 +0300

    hosting updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'public', 
+    outDir: 'dist', 
     emptyOutDir: true,
   },
   plugins: [react()],
